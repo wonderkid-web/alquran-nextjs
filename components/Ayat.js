d620789eb1dfe516d89bb296be62f441445023ba
Muhammad Wahyu Ramadhan <mwahyuap2018@gmail.com>
2023-01-15 01:53:17 +0700

    fix key

--- components/Ayat.js
+++ components/Ayat.js
@@ -2,7 +2,7 @@ import styles from "../styles/Quran.module.css";
 export default function Ayat({ surah }) {
   return surah.map((ayat) => {
     return (
-      <div className={styles.ayat}>
+      <div className={styles.ayat} key={ayat.nomor}>
         <h1 className={styles.nomorAyat}>{ayat.nomor}</h1>
         <div className={styles.ayatSection}>
           <p className={styles.ayatArabic}>{ayat.ar}</p>

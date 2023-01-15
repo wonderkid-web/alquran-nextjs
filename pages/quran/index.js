1cb962442bad388fb6fcb14e1ad1f802f7ee08b4
Muhammad Wahyu Ramadhan <mwahyuap2018@gmail.com>
2023-01-15 09:48:19 +0700

    responsive mobile

--- pages/quran/index.js
+++ pages/quran/index.js
@@ -4,8 +4,8 @@ import styles from "../../styles/Quran.module.css";
 const API_QURAN = "https://equran.id/api/surat";
 export default function Quran({ quran }) {
   return (
-    <div>
-      <h1>Read The Holly Quran below</h1>
+    <div className={styles.surahContainer}>
+      <h1 className={styles.h1}>Read The Holly Quran below</h1>
       {quran.map((surah) => {
         return (
             <Surah surah={surah} key={surah.nomor}/>
